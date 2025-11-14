0b919ff6cd6c05ada6d895d65566625e3dacde28
FrancoDev7 <francodevs01@gmail.com>
2025-11-14 02:00:33 -0300

    comit of the safe pass

---
+++ lib/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+export const db = globalThis.prisma || new PrismaClient();
+
+// esto es para evitar multiples instancias en desarrollo
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
